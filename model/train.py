d050ee701474428f4c7b6ef43e44860b244fbc72
Shaik Sajida <sajidask2006@gmail.com>
2026-06-22 16:49:06 +0530

    Remove virtual environment from repository

--- model/train.py
+++ model/train.py
@@ -1,1 +1,64 @@
-print("Model training")
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, models
+
+DATASET_PATH = "dataset"
+
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+train_data = datagen.flow_from_directory(
+    DATASET_PATH,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='categorical',
+    subset='training'
+)
+
+val_data = datagen.flow_from_directory(
+    DATASET_PATH,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='categorical',
+    subset='validation'
+)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu',
+                  input_shape=(224,224,3)),
+    layers.MaxPooling2D(2,2),
+
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+
+    layers.Conv2D(128, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+
+    layers.Flatten(),
+
+    layers.Dense(128, activation='relu'),
+    layers.Dense(3, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.summary()
+
+history = model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=5
+)
+
+model.save("crop_disease_model.keras")
+
+print("Model Saved Successfully")
